820665c27c49befdcd159ff443298d58ad901e7f
zyhut200 <99744043+zyhut200@users.noreply.github.com>
2023-10-19 16:43:20 +0800

    Update jk.sh

--- jk.sh
+++ jk.sh
@@ -40,47 +40,65 @@ case $OS in
         ;;
 esac
 
+# 安装并配置vnStat
+if [[ "$CLIENT_VNSTAT" == "yes" ]]; then
+    # vnStat 安装
+    case $OS in
+        ubuntu|debian)
+            apt install -y vnstat
+            ;;
+        centos|redhat|fedora)
+            yum install -y vnstat
+            ;;
+    esac
+    
+    # 初始化 vnStat
+    vnstat -u -i eth0
+
+    # 启动 vnStat 服务
+    case $OS in
+        ubuntu|debian|centos|redhat)
+            systemctl enable vnstat
+            systemctl start vnstat
+            ;;
+    esac
+fi
+
 # 下载客户端文件
 wget -O /usr/local/status-client.py https://raw.githubusercontent.com/cokemine/ServerStatus-Hotaru/master/clients/status-client.py
 
-# 配置客户端信息
+# 添加 vnStat 支持的代码到 status-client.py 文件
+echo "
+import os
+import json
+
+def get_vnstat_data():
+    try:
+        vnstat_output = os.popen('vnstat --json').read()
+        vnstat_data = json.loads(vnstat_output)
+        return vnstat_data
+    except Exception as e:
+        print('Error getting vnStat data:', e)
+        return None
+" >> /usr/local/status-client.py
+
+# 修改 status-client.py 文件，添加从 vnStat 获取流量数据的代码
+sed -i "/        load = 'load average:/i \
+        vnstat_data = get_vnstat_data()\n\
+        if vnstat_data:\n\
+            rx = vnstat_data['interfaces'][0]['traffic']['total']['rx']\n\
+            tx = vnstat_data['interfaces'][0]['traffic']['total']['tx']\n\
+        else:\n\
+            rx = 0\n\
+            tx = 0\n" /usr/local/status-client.py
+
+# 修改 status-client.py 文件中的 username, password, server 和 port
 sed -i "s/^SERVER =.*/SERVER = \"$CLIENT_SERVER\"/" /usr/local/status-client.py
 sed -i "s/^PORT =.*/PORT = $CLIENT_SERVER_PORT/" /usr/local/status-client.py
 sed -i "s/^USER =.*/USER = \"$CLIENT_USER\"/" /usr/local/status-client.py
 sed -i "s/^PASSWORD =.*/PASSWORD = \"$CLIENT_PASSWORD\"/" /usr/local/status-client.py
 
-# 如果启用了vnStat，配置vnStat
-if [[ "$CLIENT_VNSTAT" == "yes" ]]; then
-    if ! command -v vnstat &> /dev/null; then
-        echo "vnStat could not be found!"
-        exit
-    fi
-
-    VNSTAT_VERSION=$(vnstat --version | head -n 1 | awk '{print $2}')
-    echo "Detected vnStat version: $VNSTAT_VERSION"
-
-    case "$VNSTAT_VERSION" in 
-        1.*)
-            # 检查并启动vnStat服务 (for version 1.x)
-            service vnstat start
-            ;;
-        2.*)
-            # 检查并启动vnStat服务 (for version 2.x)
-            systemctl enable vnstat
-            systemctl start vnstat
-            ;;
-        *)
-            echo "Unsupported vnStat version"
-            exit 1
-            ;;
-    esac
-
-    # 设置cron job在每月1号重置流量
-    # Note: For vnStat 1.x, you may need to handle the database reset manually or upgrade to 2.x
-    (crontab -l 2>/dev/null; echo "0 0 1 * * vnstat --delete --force") | crontab -
-fi
-
-# 创建Systemd服务单元文件
+# 设置 Systemd 服务单元文件
 cat <<EOL > /etc/systemd/system/status-client.service
 [Unit]
 Description=Server Status Client
@@ -97,7 +115,7 @@ ExecStart=/usr/bin/python /usr/local/status-client.py run
 WantedBy=multi-user.target
 EOL
 
-# 重新加载Systemd守护进程，启动服务并使其在开机时自启
+# 启动并设置开机自启
 systemctl daemon-reload
 systemctl start status-client
 systemctl enable status-client
