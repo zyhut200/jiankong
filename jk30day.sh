54286d10b92abdfc45924df5050aaed61b1d28c7
zyhut200 <99744043+zyhut200@users.noreply.github.com>
2023-10-19 21:24:12 +0800

    Update jk30day.sh

--- jk30day.sh
+++ jk30day.sh
@@ -50,17 +50,47 @@ sed -i "s/^PORT =.*/PORT = $CLIENT_SERVER_PORT/" /usr/local/status-client.py
 sed -i "s/^USER =.*/USER = \"$CLIENT_USER\"/" /usr/local/status-client.py
 sed -i "s/^PASSWORD =.*/PASSWORD = \"$CLIENT_PASSWORD\"/" /usr/local/status-client.py
 
-# 初始化 vnStat
+
+# 初始化 vnStat 并启动服务
 if [[ "$CLIENT_VNSTAT" == "yes" ]]; then
     vnstat -i eth0
     systemctl enable vnstat
     systemctl start vnstat
 
+    # 设置cron job每2小时备份vnStat数据到 /backup/vnstat/
+    mkdir -p /backup/vnstat
+    (crontab -l 2>/dev/null; echo "0 */2 * * * cp /var/lib/vnstat/* /backup/vnstat/") | crontab -
+
     # 设置cron job每30天重置vnStat数据
     (crontab -l 2>/dev/null; echo "0 0 */30 * * vnstat --delete --force") | crontab -
 fi
 
-# 创建 Systemd 服务单元文件
+# 修改客户端文件，改为读取备份的vnStat数据
+if [[ "$CLIENT_VNSTAT" == "yes" ]]; then
+    echo "
+import json
+import os
+
+def get_backup_vnstat_data():
+    try:
+        with open('/backup/vnstat/vnstat.json', 'r') as file:
+            data = json.load(file)
+        return data
+    except Exception as e:
+        print('Error reading backup vnStat data:', e)
+        return None
+
+vnstat_data = get_backup_vnstat_data()
+if vnstat_data:
+    rx = vnstat_data['interfaces'][0]['traffic']['total']['rx']
+    tx = vnstat_data['interfaces'][0]['traffic']['total']['tx']
+else:
+    rx = 0
+    tx = 0
+" >> /usr/local/status-client.py
+fi
+
+# 创建Systemd服务单元文件
 PYTHON_COMMAND=$(command -v python3 || command -v python)
 cat <<EOL > /etc/systemd/system/status-client.service
 [Unit]
@@ -78,7 +108,7 @@ ExecStart=$PYTHON_COMMAND /usr/local/status-client.py run
 WantedBy=multi-user.target
 EOL
 
-# 重新加载 Systemd 守护进程，启动服务并使其在开机时自启
+# 重新加载Systemd守护进程，启动服务并使其在开机时自启
 systemctl daemon-reload
 systemctl start status-client
 systemctl enable status-client
