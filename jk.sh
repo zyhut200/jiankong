801f76d4e83299ca1dea918afb6085e23d712e44
zyhut200 <99744043+zyhut200@users.noreply.github.com>
2023-10-19 16:20:45 +0800

    Update jk.sh

--- jk.sh
+++ jk.sh
@@ -8,38 +8,37 @@ echo "Client IP: $CLIENT_IP"
 CLIENT_USER="$CLIENT_IP"           # 用户名
 CLIENT_PASSWORD="123456"           # 密码
 CLIENT_SERVER="45.148.134.106"     # 服务端IP地址
-CLIENT_SERVER_PORT="51000"         # 服务端监听的IP端口
+CLIENT_SERVER_PORT="51000"         # 服务端监听的端口
 CLIENT_VNSTAT="yes"                # 是否启用vnStat
 
-# 检查操作系统
-if [[ -e /etc/debian_version ]]; then
-    OS='debian'
-    INSTALLER='apt'
-elif [[ -e /etc/redhat-release ]]; then
-    OS='redhat'
-    INSTALLER='yum'
+# 检查操作系统并安装必要软件
+if [[ -f /etc/os-release ]]; then
+    . /etc/os-release
+    OS=$ID
+elif type lsb_release >/dev/null 2>&1; then
+    OS=$(lsb_release -si)
+elif [[ -f /etc/redhat-release ]]; then
+    OS="redhat"
 else
-    echo "Unsupported operating system."
-    exit 1
+    OS=$(uname -s)
 fi
 
-# 更新系统并安装必要的软件包
-$INSTALLER update -y
-$INSTALLER install -y python python-pip wget
+echo "Detected operating system: $OS"
 
-# 检查vnStat是否安装
-if ! command -v vnstat &> /dev/null
-then
-    echo "vnStat could not be found!"
-    # Debian/Ubuntu安装vnStat
-    if [[ $OS == 'debian' ]]; then
-        $INSTALLER install -y vnstat
-    # CentOS安装vnStat
-    elif [[ $OS == 'redhat' ]]; then
-        $INSTALLER install epel-release -y
-        $INSTALLER install vnstat -y
-    fi
-fi
+case $OS in
+    ubuntu|debian)
+        apt update
+        apt install -y python python-pip wget vnstat
+        ;;
+    centos|redhat|fedora)
+        yum update -y
+        yum install -y python python-pip wget vnstat
+        ;;
+    *)
+        echo "Unsupported operating system $OS"
+        exit 1
+        ;;
+esac
 
 # 下载客户端文件
 wget -O /usr/local/status-client.py https://raw.githubusercontent.com/cokemine/ServerStatus-Hotaru/master/clients/status-client.py
@@ -51,11 +50,34 @@ sed -i "s/^USER =.*/USER = \"$CLIENT_USER\"/" /usr/local/status-client.py
 sed -i "s/^PASSWORD =.*/PASSWORD = \"$CLIENT_PASSWORD\"/" /usr/local/status-client.py
 
 # 如果启用了vnStat，配置vnStat
-if [ "$CLIENT_VNSTAT" = "yes" ]; then
-    INTERFACE=$(vnstat --iflist | grep -E -v "lo" | head -n 1)
-    vnstat --create -i "$INTERFACE"
-    systemctl enable vnstat
-    systemctl start vnstat
+if [[ "$CLIENT_VNSTAT" == "yes" ]]; then
+    if ! command -v vnstat &> /dev/null; then
+        echo "vnStat could not be found!"
+        exit
+    fi
+
+    VNSTAT_VERSION=$(vnstat --version | head -n 1 | awk '{print $2}')
+    echo "Detected vnStat version: $VNSTAT_VERSION"
+
+    case "$VNSTAT_VERSION" in 
+        1.*)
+            # 检查并启动vnStat服务 (for version 1.x)
+            service vnstat start
+            ;;
+        2.*)
+            # 检查并启动vnStat服务 (for version 2.x)
+            systemctl enable vnstat
+            systemctl start vnstat
+            ;;
+        *)
+            echo "Unsupported vnStat version"
+            exit 1
+            ;;
+    esac
+
+    # 设置cron job在每月1号重置流量
+    # Note: For vnStat 1.x, you may need to handle the database reset manually or upgrade to 2.x
+    (crontab -l 2>/dev/null; echo "0 0 1 * * vnstat --delete --force") | crontab -
 fi
 
 # 创建Systemd服务单元文件
@@ -80,4 +102,4 @@ systemctl daemon-reload
 systemctl start status-client
 systemctl enable status-client
 
-echo "ServerStatus client installation completed!"
+echo "Client setup completed!"
