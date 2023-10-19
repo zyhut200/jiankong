57ca6c6ccfdce835c0fe39115a663af16f8846ef
zyhut200 <99744043+zyhut200@users.noreply.github.com>
2023-10-19 14:07:22 +0800

    Update fw.sh

--- fw.sh
+++ fw.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# ServerStatus配置文件路径
+# ServerStatus 配置文件路径
 CONFIG_FILE="/usr/local/ServerStatus/server/config.json"
 
 # 备份当前的配置文件
@@ -8,7 +8,7 @@ cp "$CONFIG_FILE" "$CONFIG_FILE.bak"
 
 # 函数用于添加服务器状态节点
 add_server_status_node() {
-    local username password name virtualization location region
+    local username password name type location region
     IFS=',' read -r -a node_info <<< "$1"
     
     # 解析节点信息
@@ -17,35 +17,41 @@ add_server_status_node() {
             username=*) username="${info#*=}" ;;
             password=*) password="${info#*=}" ;;
             name=*) name="${info#*=}" ;;
-            virtualization=*) virtualization="${info#*=}" ;;
+            type=*) type="${info#*=}" ;;
             location=*) location="${info#*=}" ;;
             region=*) region="${info#*=}" ;;
         esac
     done
     
     # 构建JSON格式的节点信息并添加到配置文件
-    node_json="{
-        \"username\": \"$username\",
-        \"password\": \"$password\",
-        \"name\": \"$name\",
-        \"type\": \"$virtualization\",
-        \"location\": \"$location\",
-        \"region\": \"$region\"
-    }"
+    node_json=$(cat <<- EOM
+        {
+            "username": "$username",
+            "password": "$password",
+            "name": "$name",
+            "type": "$type",
+            "host": "None",
+            "location": "$location",
+            "disabled": false,
+            "region": "$region"
+        }
+    EOM
+    )
 
-    # 使用jq工具将新的节点信息添加到配置文件的json数组中
-    # 安装jq工具如果你的系统还没有安装
+    # 使用 jq 工具将新的节点信息添加到配置文件的 json 数组中
+    # 安装 jq 工具如果你的系统还没有安装
     # sudo apt install jq (Debian/Ubuntu)
     # sudo yum install jq (CentOS)
 
     jq ".servers += [$node_json]" "$CONFIG_FILE" > "temp.json" && mv "temp.json" "$CONFIG_FILE"
 }
 
-echo "Please enter your nodes' information (each line for a node, Ctrl-D to end):"
+echo "请按格式输入您的节点信息 (每行一个节点，Ctrl-D 结束输入)："
+echo "格式：username=用户名,password=密码,name=名称,type=类型,location=位置,region=地区"
 
 # 读取多行输入
 while IFS= read -r line; do
     add_server_status_node "$line"
 done
 
-echo "Nodes have been added successfully!"
+echo "节点已成功添加！"
