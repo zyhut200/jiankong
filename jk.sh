a988bbbd622e0467bf334899317f1988d993222e
zyhut200 <99744043+zyhut200@users.noreply.github.com>
2023-10-19 12:09:21 +0800

    Update jk.sh

--- jk.sh
+++ jk.sh
@@ -1,58 +1,98 @@
 #!/bin/bash
+# Auto install the ServerStatus client
+# Only for CentOS/Debian/Ubuntu
 
-# 预设的参数
-CLIENT_SERVER_IP="45.148.134.106"    # 服务器 IP 地址
-CLIENT_PORT="51000"                  # 服务器端口号
-CLIENT_USER="root"                   # 用户名
-CLIENT_PASSWORD="@Zy123456789"       # 密码
-CLIENT_VNSTAT="yes"                  # 是否启用 vnStat
+CLIENT_SERVER="45.148.134.106"
+CLIENT_PORT="51000"
+CLIENT_USER=$(curl -s icanhazip.com) # 自动获取服务器IP
+CLIENT_PASSWORD="123456" # 密码
+CLIENT_VNSTAT="yes" # 是否启用vnStat
 
-# 检查是否为root用户
-[ $(id -u) != "0" ] && {
-    echo "错误: 你必须以root用户来执行此安装脚本"
+# Check if user is root
+[ $(id -u) != "0" ] && { echo "Error: You must be root to run this script"; exit 1; }
+
+# Check OS type
+if [ -e /etc/redhat-release ]; then
+    OS='CentOS'
+elif cat /etc/issue | grep -q -E -i "debian"; then
+    OS='Debian'
+elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+    OS='Ubuntu'
+else
+    echo "Not support OS, Please reinstall OS and retry!"
     exit 1
-}
-
-# 安装依赖
-if [[ ${release} == "centos" ]]; then
-    yum update -y
-    yum install -y python make wget
-else
-    apt-get update -y
-    apt-get install -y python make wget
 fi
 
-# 下载客户端文件
-wget --no-check-certificate -O client-linux.py "https://raw.githubusercontent.com/cppla/ServerStatus/master/clients/client-linux.py"
+# Install dependencies
+if [ ${OS} == 'CentOS' ]; then
+    yum -y install python make wget
+elif [ ${OS} == 'Debian' -o ${OS} == 'Ubuntu' ]; then
+    apt-get -y install python make wget
+fi
 
-# 设置客户端配置
-sed -i "s/SERVER = '127.0.0.1'/SERVER = '$CLIENT_SERVER_IP'/g" client-linux.py
-sed -i "s/PORT = 35601/PORT = $CLIENT_PORT/g" client-linux.py
-sed -i "s/USER = 'USER'/USER = '$CLIENT_USER'/g" client-linux.py
-sed -i "s/PASSWORD = 'PASSWORD'/PASSWORD = '$CLIENT_PASSWORD'/g" client-linux.py
+# Download ServerStatus client
+cd /usr/local/
+wget --no-check-certificate https://github.com/cppla/ServerStatus/raw/master/clients/client-linux.py -O status-client.py
+[ ! -e "status-client.py" ] && { echo "Error: Download client failed."; exit 1; }
 
-# 检查并安装vnStat
-if [[ $CLIENT_VNSTAT == "yes" ]]; then
-    if [[ ! -e '/usr/bin/vnstat' ]]; then
-        if [[ ${release} == "centos" ]]; then
-            yum install -y vnstat
-        else
-            apt-get install -y vnstat
-        fi
+# Create config file
+cat > /usr/local/status-client.conf << EOF
+SERVER = "${CLIENT_SERVER}"
+PORT = ${CLIENT_PORT}
+USER = "${CLIENT_USER}"
+PASSWORD = "${CLIENT_PASSWORD}"
+EOF
+
+# Create service
+if [ ${OS} == 'CentOS' ]; then
+    if [ ! -e /etc/init.d/status-client ]; then
+        cat > /etc/init.d/status-client << EOF
+#!/bin/sh
+# chkconfig:   2345 50 50
+# description:  Server Status client
+
+### BEGIN INIT INFO
+# Provides:          ServerStatus
+# Required-Start:    \$local_fs \$remote_fs \$network
+# Required-Stop:     \$local_fs \$remote_fs \$network
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+### END INIT INFO
+
+cd /usr/local
+python /usr/local/status-client.py /usr/local/status-client.conf run
+EOF
+        chmod +x /etc/init.d/status-client
+        chkconfig --add status-client
+        chkconfig status-client on
     fi
-    sed -i "s/vnstat_enabled = False/vnstat_enabled = True/g" client-linux.py
+elif [ ${OS} == 'Debian' -o ${OS} == 'Ubuntu' ]; then
+    if [ ! -e /etc/systemd/system/status-client.service ]; then
+        cat > /etc/systemd/system/status-client.service << EOF
+[Unit]
+Description=ServerStatus client
+Documentation=https://github.com/cppla/ServerStatus
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/python /usr/local/status-client.py /usr/local/status-client.conf run
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=4096
+
+[Install]
+WantedBy=multi-user.target
+EOF
+        systemctl enable status-client
+    fi
 fi
 
-# 设置开机自启
-if [[ ${release} == "centos" ]]; then
-    echo -e "\nnohup python $(pwd)/client-linux.py &" >> /etc/rc.d/rc.local
-    chmod +x /etc/rc.d/rc.local
-else
-    echo -e "\nnohup python $(pwd)/client-linux.py &" >> /etc/rc.local
-    chmod +x /etc/rc.local
+# Start service
+if [ ${OS} == 'CentOS' ]; then
+    service status-client start
+elif [ ${OS} == 'Debian' -o ${OS} == 'Ubuntu' ]; then
+    systemctl start status-client
 fi
 
-# 启动客户端
-nohup python $(pwd)/client-linux.py &
-
-echo "客户端安装并启动完成!"
+echo "ServerStatus client has been installed and started successfully!"
