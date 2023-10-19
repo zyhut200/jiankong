168b5331c1f259569d84e5b0e1a6c4c85e463110
zyhut200 <99744043+zyhut200@users.noreply.github.com>
2023-10-19 13:05:38 +0800

    Update jk.sh

--- jk.sh
+++ jk.sh
@@ -1,98 +1,75 @@
 #!/bin/bash
-# Auto install the ServerStatus client
-# Only for CentOS/Debian/Ubuntu
 
-CLIENT_SERVER="45.148.134.106"
-CLIENT_PORT="51000"
-CLIENT_USER=$(curl -s icanhazip.com) # 自动获取服务器IP
-CLIENT_PASSWORD="123456" # 密码
-CLIENT_VNSTAT="yes" # 是否启用vnStat
+# 获取服务器的公网IP地址
+CLIENT_IP=$(curl -s https://api.ipify.org)
 
-# Check if user is root
-[ $(id -u) != "0" ] && { echo "Error: You must be root to run this script"; exit 1; }
+# 如果获取IP地址失败，就尝试用另一个API再获取一次
+if [ -z "$CLIENT_IP" ]; then
+    CLIENT_IP=$(curl -s https://ipinfo.io/ip)
+fi
 
-# Check OS type
-if [ -e /etc/redhat-release ]; then
-    OS='CentOS'
-elif cat /etc/issue | grep -q -E -i "debian"; then
-    OS='Debian'
-elif cat /etc/issue | grep -q -E -i "ubuntu"; then
-    OS='Ubuntu'
-else
-    echo "Not support OS, Please reinstall OS and retry!"
+# 如果再次失败，就输出错误信息并退出脚本
+if [ -z "$CLIENT_IP" ]; then
+    echo "Error: Unable to get the public IP address of this server."
     exit 1
 fi
 
-# Install dependencies
-if [ ${OS} == 'CentOS' ]; then
-    yum -y install python make wget
-elif [ ${OS} == 'Debian' -o ${OS} == 'Ubuntu' ]; then
-    apt-get -y install python make wget
+# 将获取的IP地址赋值给 CLIENT_USER 变量
+CLIENT_USER="$CLIENT_IP"
+
+# 其他用户配置信息
+CLIENT_SERVER="45.148.134.106"
+CLIENT_PORT="51000"
+CLIENT_PASSWORD="123456"
+
+# 安装依赖
+apt-get update
+apt-get install -y python3 wget vnstat
+
+# 下载客户端脚本
+wget -N --no-check-certificate https://raw.githubusercontent.com/cokemine/ServerStatus-Hotaru/master/clients/status-client.py -O /usr/local/status-client.py
+
+# 如果文件下载失败，则输出错误信息并退出脚本
+if [ ! -e '/usr/local/status-client.py' ]; then
+    echo "Error: Failed to download client script."
+    exit 1
 fi
 
-# Download ServerStatus client
-cd /usr/local/
-wget --no-check-certificate https://github.com/cppla/ServerStatus/raw/master/clients/client-linux.py -O status-client.py
-[ ! -e "status-client.py" ] && { echo "Error: Download client failed."; exit 1; }
+# 替换客户端脚本中的配置信息
+sed -i "s/SERVER = .*/SERVER = \"$CLIENT_SERVER\"/" /usr/local/status-client.py
+sed -i "s/PORT = .*/PORT = $CLIENT_PORT/" /usr/local/status-client.py
+sed -i "s/USER = .*/USER = \"$CLIENT_USER\"/" /usr/local/status-client.py
+sed -i "s/PASSWORD = .*/PASSWORD = \"$CLIENT_PASSWORD\"/" /usr/local/status-client.py
 
-# Create config file
-cat > /usr/local/status-client.conf << EOF
-SERVER = "${CLIENT_SERVER}"
-PORT = ${CLIENT_PORT}
-USER = "${CLIENT_USER}"
-PASSWORD = "${CLIENT_PASSWORD}"
-EOF
-
-# Create service
-if [ ${OS} == 'CentOS' ]; then
-    if [ ! -e /etc/init.d/status-client ]; then
-        cat > /etc/init.d/status-client << EOF
-#!/bin/sh
-# chkconfig:   2345 50 50
-# description:  Server Status client
-
-### BEGIN INIT INFO
-# Provides:          ServerStatus
-# Required-Start:    \$local_fs \$remote_fs \$network
-# Required-Stop:     \$local_fs \$remote_fs \$network
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-### END INIT INFO
-
-cd /usr/local
-python /usr/local/status-client.py /usr/local/status-client.conf run
-EOF
-        chmod +x /etc/init.d/status-client
-        chkconfig --add status-client
-        chkconfig status-client on
-    fi
-elif [ ${OS} == 'Debian' -o ${OS} == 'Ubuntu' ]; then
-    if [ ! -e /etc/systemd/system/status-client.service ]; then
-        cat > /etc/systemd/system/status-client.service << EOF
+# 创建系统服务
+cat > /etc/systemd/system/status-client.service << EOF
 [Unit]
 Description=ServerStatus client
-Documentation=https://github.com/cppla/ServerStatus
 After=network.target
 
 [Service]
 Type=simple
-ExecStart=/usr/bin/python /usr/local/status-client.py /usr/local/status-client.conf run
 Restart=on-failure
-RestartSec=3
-LimitNOFILE=4096
+RestartSec=5s
+ExecStart=/usr/bin/python3 /usr/local/status-client.py
 
 [Install]
 WantedBy=multi-user.target
 EOF
-        systemctl enable status-client
-    fi
-fi
 
-# Start service
-if [ ${OS} == 'CentOS' ]; then
-    service status-client start
-elif [ ${OS} == 'Debian' -o ${OS} == 'Ubuntu' ]; then
-    systemctl start status-client
-fi
+# 重载系统服务、启动客户端服务、设置开机启动
+systemctl daemon-reload
+systemctl start status-client
+systemctl enable status-client
 
-echo "ServerStatus client has been installed and started successfully!"
+# 输出客户端信息
+echo "————————————————————"
+echo "  ServerStatus 客户端配置信息："
+echo "  IP       : ${CLIENT_IP}"
+echo "  端口     : ${CLIENT_PORT}"
+echo "  用户名   : ${CLIENT_USER}"
+echo "  密码     : ${CLIENT_PASSWORD}"
+echo "————————————————————"
+
+# 完成
+echo "ServerStatus 客户端安装完成！"
