07b76d8afb3a154308d73bdf98218231d7b09b34
zyhut200 <99744043+zyhut200@users.noreply.github.com>
2023-10-19 14:33:00 +0800

    Update jk.sh

--- jk.sh
+++ jk.sh
@@ -1,54 +1,67 @@
 #!/bin/bash
 
-# 自动获取本机公网IP
+# 获取本机IP地址
 CLIENT_IP=$(curl -s https://api.ipify.org)
 
-# 客户端设置
-CLIENT_USER="$CLIENT_IP"                   # 用户名
-CLIENT_PASSWORD="123456"                   # 密码
-CLIENT_SERVER="45.148.134.106"             # 服务端IP地址
-CLIENT_SERVER_PORT="51000"                 # 服务端监听的IP端口
-CLIENT_VNSTAT="yes"                        # 是否启用vnStat
+# 定义服务器和客户端设置
+SERVER="45.148.134.106"
+SERVER_PORT=51000
+CLIENT_USER="$CLIENT_IP"
+CLIENT_PASSWORD="123456"
+CLIENT_VNSTAT="yes" # 是否启用vnStat
 
-# 安装依赖
-apt-get update
-apt-get install -y python python-pip
-pip install psutil
-
-# 下载客户端脚本
-cd /usr/local
-wget https://raw.githubusercontent.com/cokemine/ServerStatus-Hotaru/master/clients/status-client.py
-chmod 755 status-client.py
-
-# 创建客户端配置文件
-cat > /usr/local/status-client.conf << EOF
-# Config
-SERVER = "$CLIENT_SERVER"
-PORT = $CLIENT_SERVER_PORT
-USER = "$CLIENT_USER"
-PASSWORD = "$CLIENT_PASSWORD"
-INTERVAL = 1
-EOF
-
-# 如果启用vnStat, 需要进行安装
-if [ "$CLIENT_VNSTAT" = "yes" ]; then
-    apt-get install -y vnstat
+# 检测操作系统并安装必要的软件包
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS=$NAME
+elif type lsb_release >/dev/null 2>&1; then
+    OS=$(lsb_release -si)
+elif [ -f /etc/lsb-release ]; then
+    . /etc/lsb-release
+    OS=$DISTRIB_ID
+elif [ -f /etc/debian_version ]; then
+    OS=Debian
+elif [ -f /etc/redhat-release ]; then
+    OS=$(cat /etc/redhat-release | cut -f1 -d" ")
+else
+    OS=$(uname -s)
 fi
 
-# 创建系统服务
-cat > /etc/systemd/system/status-client.service << EOF
-[Unit]
-Description=Server Status client
+# 安装依赖和客户端
+case $OS in
+    Ubuntu*|Debian*)
+        apt-get update
+        apt-get install -y python3 python3-pip vnstat wget
+        ;;
+    CentOS*|Fedora*|Red*)
+        yum update -y
+        yum install -y python3 python3-pip vnstat wget
+        ;;
+    *)
+        echo "Unsupported operating system $OS"
+        exit 1
+        ;;
+esac
 
-[Service]
-WorkingDirectory=/usr/local
-ExecStart=/usr/bin/python /usr/local/status-client.py /usr/local/status-client.conf run
-Restart=always
+# 下载并修改客户端配置文件
+wget -O /usr/local/status-client.py https://raw.githubusercontent.com/cokemine/ServerStatus-Hotaru/master/clients/status-client.py
+wget -O /usr/local/status-client.conf https://raw.githubusercontent.com/cokemine/ServerStatus-Hotaru/master/clients/status-client.conf
 
-[Install]
-WantedBy=multi-user.target
-EOF
+# 修改客户端配置文件
+sed -i "s/SERVER = .*/SERVER = \"$SERVER\"/g" /usr/local/status-client.py
+sed -i "s/PORT = .*/PORT = $SERVER_PORT/g" /usr/local/status-client.py
+sed -i "s/USER = .*/USER = \"$CLIENT_USER\"/g" /usr/local/status-client.py
+sed -i "s/PASSWORD = .*/PASSWORD = \"$CLIENT_PASSWORD\"/g" /usr/local/status-client.py
 
-# 启动系统服务
-systemctl enable status-client
-systemctl start status-client
+# 如果启用了vnStat，配置vnStat
+if [ "$CLIENT_VNSTAT" = "yes" ]; then
+    vnstat --create -i eth0
+    systemctl enable vnstat
+    systemctl start vnstat
+fi
+
+# 添加到开机启动
+echo "@reboot root /usr/bin/python3 /usr/local/status-client.py /usr/local/status-client.conf run" >> /etc/crontab
+
+# 运行客户端
+python3 /usr/local/status-client.py /usr/local/status-client.conf run
